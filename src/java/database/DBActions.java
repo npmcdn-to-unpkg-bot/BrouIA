3f7146b8216405783db98ddad78e34641f54a57a
JohnnyJosep <johnnyjosep@hotmail.com>
2016-01-22 11:20:49 +0100

    mes brou

--- src/java/database/DBActions.java
+++ src/java/database/DBActions.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package database;
 
-/**
- *
- * @author johnn
- */
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class DBActions {
-    
+    public ArrayList <String> getTendencias() {
+        DBConnection con = new DBConnection();
+        ArrayList <String> res = new ArrayList <String> ();
+        try {
+            con.open();
+            Statement st = con.getConection().createStatement();
+            ResultSet rs = st.executeQuery("select * from sm_tendencias;");
+            String aux;
+            while (rs.next()) {
+                aux = rs.getString("nom_tendencia");
+                aux = aux + "##ltim##" + rs.getInt("total_ocurrencias");
+                res.add(aux);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            con.close();
+        }
+        return res;
+    }
 }
--- src/java/database/DBProperties.java
+++ src/java/database/DBProperties.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package database;
 
-/**
- *
- * @author johnn
- */
 public class DBProperties {
-    
+    static public final String host = "localhost";
+    static public final String port = "3306";
+    static public final String db = "adiiu";
+    static public final String user = "adiiu";
+    static public final String pass = "password";
 }
